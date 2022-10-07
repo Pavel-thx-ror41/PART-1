9687f80cf03a39708208b7afccbaac36cf2b1d1a
Pavel-thx-ror41 <todoer@yandex.ru>
2022-10-07 11:13:10 +0300

    PART1-P5-OOP wip 5

--- P5_OOP/main.rb
+++ P5_OOP/main.rb
@@ -7,29 +7,29 @@ require_relative 'rail_way.rb'
 
 MENU = [
   {
-    command: "I", description: "Диспетчерская",
+    command: "Д", description: "Диспетчерская",
     object_show: "@railway" },
   {
-    command: "SL", description: "Просмотреть список станций",
+    command: "С?", description: "Просмотреть список станций",
     list_titles: "@railway.stations"
   },
   {
-    command: "SC", description: "Создать станцию, например: \033[1mSC Москва\033[0m",
-    object_create: "Station",  params: { "title" => "" }, target_list: "@railway.stations"
+    command: "С+", description: "Создать станцию, например: \033[1mSC Москва\033[0m",
+    object_create: "Station",  object_create_params: { "title" => "" }, target_list: "@railway.stations"
   },
   {
-    command: "ST", description: "Просмотреть список поездов на станции, например: \033[1mSТ Москва\033[0m",
+    command: "СП", description: "Просмотреть список поездов на станции, например: \033[1mSТ Москва\033[0m или \033[1mSТ\033[0m для всех",
     source_list: "@railway.stations", source_list_filter: { "title" => "" }, object_list_method: { "trains_get" => "number_get"}
   },
-  # { command: :TC,  description: "Создавать поезда", params: "", list: nil },
-  # { command: :TRS, description: "Назначать маршрут поезду", params: "", list: nil },
-  # { command: :TRF, description: "Перемещать поезд по маршруту вперед и", params: "", list: nil },
-  # { command: :TRB, description: "Перемещать поезд по маршруту назад", params: "", list: nil },
+  # { command: :П+,  description: "Создавать поезда", params: "", list: nil },
+  # { command: :ПМ, description: "Назначать маршрут поезду", params: "", list: nil },
+  # { command: :ПМВ, description: "Перемещать поезд по маршруту вперед и", params: "", list: nil },
+  # { command: :ПМН, description: "Перемещать поезд по маршруту назад", params: "", list: nil },
   #
-  # { command: :TWA, description: "Добавлять вагоны к поезду", params: "", list: nil },
-  # { command: :TWR, description: "Отцеплять вагоны от поезда", params: "", list: nil },
+  # { command: :ПВ+, description: "Добавлять вагоны к поезду", params: "", list: nil },
+  # { command: :ПВ+, description: "Отцеплять вагоны от поезда", params: "", list: nil },
   #
-  # { command: :RC,  description: "Создавать маршруты и ", params: "", list: nil },
+  # { command: :МC,  description: "Создавать маршруты и ", params: "", list: nil },
   # { command: :RSV, description: "управлять станциями в нем (добавлять, удалять)", params: "", list: nil },
   # { command: :RSA, description: "управлять станциями в нем (добавлять, удалять)", params: "", list: nil },
   # { command: :RSR, description: "управлять станциями в нем (добавлять, удалять)", params: "", list: nil }
@@ -38,7 +38,7 @@ MENU = [
 
 MENU_HELP = MENU.map do |mi|
   "  \033[1m#{mi[:command].to_s}\033[0m\t#{mi[:description].to_s}"
-  # + ( mi[:params] ? " (#{mi[:params].keys.join(", ").to_s})" : "" )
+  # + ( mi[:object_create_params] ? " (#{mi[:object_create_params].keys.join(", ").to_s})" : "" )
 end
 
 @railway = RailWay.new(seed: true)
@@ -51,7 +51,8 @@ loop do
   puts
   puts "\033[0;47;30mКоманда\tОписание\033[0m"
   puts MENU_HELP
-  puts "  \033[1mQ\033[0m\tВыход"
+  command_exit = "В"
+  puts "  \033[1m#{command_exit}\033[0m\tВыход"
   puts
   print "введите команду: "
   if command == ""
@@ -60,7 +61,7 @@ loop do
   end
   puts "\e[H\e[2J"
 
-  break if command == "Q"
+  break if command == command_exit
 
   menu_selected = MENU.find { |mi| mi[:command] == command.to_s }
 
@@ -73,23 +74,24 @@ loop do
   puts
   puts "\033[1;43;37m #{menu_selected[:description]}\033[0m\033[1;43;37m: \033[0m "
 
+
   if menu_selected[:object_show]
-    # object_show: "@railway" },
+    # object_show: "@railway" }
     eval(menu_selected[:object_show]).show
     command = ""
 
+
   elsif menu_selected[:object_create]
-    # object_create: "Station",  params: { "title" => "" }, target_list: "@railway.stations"
-    params_keys = menu_selected[:params].keys.map(&:to_s) if menu_selected[:params]
-    params_count = params_keys.count
-    params_mods = menu_selected[:params].values.map(&:to_s) if menu_selected[:params]
-    input_params_values = input.partition(' ').last.squeeze(' ').delete(";").split(",").map(&:strip).reject(&:empty?)
-
-    eval_command = ""
-    eval_command += "#{menu_selected[:target_list]} << " if menu_selected[:target_list]
+    # object_create: "Station",  object_create_params: { "title" => "" }, target_list: "@railway.stations"
+    eval_command = menu_selected[:target_list] ? "#{menu_selected[:target_list]} << " : ""
     eval_command += "#{menu_selected[:object_create]}.new"
-    eval_command += "(#{ input_params_values[..params_count].zip(params_mods).map { |i| "\""+i[0]+"\""+i[1] }.join(", ") })" if params_keys
-
+    if menu_selected[:object_create_params]
+      params_count = menu_selected[:object_create_params].keys.map(&:to_s).count
+      params_mods = menu_selected[:object_create_params].values.map(&:to_s) # to_i, to_f ...
+      input_params_values = input.partition(' ').last.squeeze(' ').delete(";").split(",").map(&:strip).reject(&:empty?)
+      constructor_params = input_params_values[..params_count].zip(params_mods).map { |i| "\""+i[0]+"\""+i[1] }.join(", ")
+      eval_command += "(#{ constructor_params })"
+    end
     eval(eval_command)
     command = "I"
 
@@ -98,11 +100,11 @@ loop do
     # list_titles: "@railway.stations"
     eval(menu_selected[:list_titles]).each { |item| puts " #{item.title}" }
     command = ""
-    # TODO возможно дублирует menu_selected[:source_list] без параметров
+    # TODO возможно дублирует menu_selected[:source_list] без параметров, проработать
     # + ,source_list_method: :"title"
     # = source_list: "@railway.stations", source_list_filter: { "title" => "" }
 
-  elsif menu_selected[:source_list] # TODO && source_list_filter && object_list_method
+  elsif menu_selected[:source_list]
     # source_list: "@railway.stations", source_list_filter: { "title" => "" }, object_list_method: { "trains_get" => "number_get"}
 
     # source_list
@@ -110,8 +112,8 @@ loop do
 
     # source_list_filter
     input_params_values = input.partition(' ').last.squeeze(' ').delete(";").split(",").map(&:strip).reject(&:empty?)
-    # input_params_values = ["Воронеж", "Москва"] # TODO remove after DEBUG
-    # params_mods = menu_selected[:params].values.map(&:to_s) if menu_selected[:params] # TODO check .to_i .to_s для не String'ов
+    input_params_values = ["Воронеж", "Москва"] # TODO remove after DEBUG
+    # params_mods = menu_selected[:object_create_params].values.map(&:to_s) if menu_selected[:object_create_params] # TODO check .to_i .to_s для не String'ов
     source_list_filter_key = menu_selected[:source_list_filter].keys.map(&:to_s).first if menu_selected[:source_list_filter]
     if source_list_filter_key && (input_params_values.count > 0)
       eval_command += ".select {|source_list_item| #{input_params_values.to_s}.include?(source_list_item.#{source_list_filter_key})}"
@@ -123,11 +125,12 @@ loop do
       puts " \033[1;43;37m #{source_list_item.send(source_list_filter_key)} \033[0m"
       puts
 
-      >>>
+      # TODO && source_list_filter && object_list_method
+      #>>>
       # WIP TODO выводим     object_list_method: { "trains_get" => "number_get"}
       # eval_command =
     end
-    binding.pry
+    #binding.pry
 
     command = ""
   else
--- P5_OOP/rail_way.rb
+++ P5_OOP/rail_way.rb
@@ -1,7 +1,7 @@
 require_relative 'station.rb'
 require_relative 'route.rb'
+require_relative 'train.rb'
 # require_relative 'wagon.rb'
-# require_relative 'train.rb'
 # require_relative 'cargo_train.rb'
 # require_relative 'passenger_train.rb'
 
