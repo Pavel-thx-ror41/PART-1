c23e793db2c021e8b0662a9207aff01a466e764a
Pavel-thx-ror41 <todoer@yandex.ru>
2022-10-26 01:22:15 +0300

    PART1-P5-OOP fix station trains by type

--- P5_OOP/station.rb
+++ P5_OOP/station.rb
@@ -1,9 +1,7 @@
 require_relative 'train.rb'
 
-# Станция
 class Station
 
-  # Имеет название, которое указывается при ее создании
   def initialize(title)
     if title.to_s.length > 0
       @title = title
@@ -17,7 +15,6 @@ class Station
     @title
   end
 
-  # Может принимать поезда (по одному за раз)
   def train_arrive(train)
     if train.is_a?(Train) && !@trains.index(train)
       @trains << train
@@ -28,18 +25,12 @@ class Station
     end
   end
 
-  # Может возвращать список всех поездов на станции, находящиеся в текущий момент
   def trains_get
     @trains
   end
 
-  # Может возвращать список поездов на станции по типу (см. ниже): кол-во грузовых, пассажирских
   def trains_get_by_type(type)
-    if Train::TYPES.include?(type)
-      @trains.select { |train| train.type_get == type }
-    else
-      raise "Ошибка данных, неправильный тип состава: #{type}, возможны только: #{Train::TYPES}"
-    end
+    @trains.select { |train| train.type_get == type }
   end
 
   # Может отправлять поезда (по одному за раз, при этом, поезд удаляется из списка поездов, находящихся на станции).
